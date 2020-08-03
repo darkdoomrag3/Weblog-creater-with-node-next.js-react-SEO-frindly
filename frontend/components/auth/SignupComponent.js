49b257f16395be755ef07da585c3bbb9e5503d63
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-08-03 19:23:49 +0430

    fix router on signup

--- frontend/components/auth/SignupComponent.js
+++ frontend/components/auth/SignupComponent.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { signup, isAuth } from '../../actions/auth';
-
+import Router from 'next/router'
 const SignupComponent = () => {
     const [values, setValues] = useState({
         name: 'Emad',
