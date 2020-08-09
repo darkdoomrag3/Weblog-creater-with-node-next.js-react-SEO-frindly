53d41202b9c7068e0615d3b55b75ddd3d0aed6a3
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-08-09 20:35:52 +0430

    add blog controllers test

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { create } = require('../controllers/blog');
+
 const { requireSignin, adminMiddleware } = require('../controllers/auth');
-router.post('/', requireSignin, adminMiddleware, create);
+
+router.post('/blog', requireSignin, adminMiddleware, create);
 
 module.exports = router;
