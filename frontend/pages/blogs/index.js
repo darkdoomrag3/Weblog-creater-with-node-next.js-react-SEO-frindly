8338d3ac5d1466d8f1f4366ad077e1185512c082
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-08-11 18:39:34 +0430

    seprate blog in cart component

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -6,54 +6,18 @@ import { listBlogsWithCategoriesAndTags } from '../../actions/blog';
 import { API } from '../../config';
 import renderHTML from 'react-render-html';
 import moment from 'moment';
+import Card from '../../components/blog/Card';
 
 const Blogs = ({ blogs, categories, tags, size }) => {
     const showAllBlogs = () => {
-
         return blogs.map((blog, i) => {
-
+            // ()
             return (
                 <article key={i}>
-                    <div className="lead pb-4">
-                        <header>
-                            <Link href={`/blogs/${blog.slug}`}>
-                                <a>
-                                    <h2 className="pt-3 pb-3 font-weight-bold">{blog.title}</h2>
-                                </a>
-                            </Link>
-                        </header>
-                        <section>
-                            <p className="mark ml-1 pt-2 pb-2">
-                                Written by {blog.postedBy.name} | Published {moment(blog.updatedAt).fromNow()}
-                            </p>
-                        </section>
-                        <section>
-                            <p>blog categories and tags</p>
-                        </section>
-
-                        <div className="row">
-                            <div className="col-md-4">image</div>
-                            <div className="col-md-8">
-                                <section>
-
-                                    {   /*
-                                    <div className="pb-3">{renderHTML(blog.excerpt)}</div>
-                            */
-                                    }
-
-                                    <div className="pb-3">{blog.excerpt}</div>
-
-                                    <Link href={`/blogs/${blog.slug}`}>
-                                        <a className="btn btn-primary pt-2">Read more</a>
-                                    </Link>
-                                </section>
-                            </div>
-                        </div>
-                    </div>
+                    <Card blog={blog} />
                     <hr />
                 </article>
             );
-
         });
     };
 
@@ -63,7 +27,7 @@ const Blogs = ({ blogs, categories, tags, size }) => {
                 <div className="container-fluid">
                     <header>
                         <div className="col-md-12 pt-3">
-                            <h1 className="display-4 font-weight-bold text-center">Programming blogs and tutorials</h1>
+                            <h1 className="display-4 font-weight-bold text-center">Programming blogs</h1>
                         </div>
                         <section>
                             <p>show categories and tags</p>
