ba34cd355950c752e1eb3cd8ab20a2d4fe248150
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-08-12 20:23:06 +0430

    complite dynamic slug and related category in front and back

--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -1,13 +1,49 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import moment from 'moment';
 import Layout from '../../components/Layout';
-import { useState } from 'react';
-import { singleBlog } from '../../actions/blog';
+import { useState, useEffect } from 'react';
+import { singleBlog, listRelated } from '../../actions/blog';
 import { API, DOMAIN, APP_NAME } from '../../config';
 import renderHTML from 'react-render-html';
+import moment from 'moment';
+import SmallCard from '../../components/blog/SmallCard';
 
-const SingleBlog = ({ blog, router }) => {
+const SingleBlog = ({ blog, query }) => {
+    const [related, setRelated] = useState([]);
+
+    const loadRelated = () => {
+        listRelated({ blog }).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setRelated(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadRelated();
+    }, []);
+
+    const head = () => (
+        <Head>
+            <title>
+                {blog.title} | {APP_NAME}
+            </title>
+            <meta name="description" content={blog.mdesc} />
+            <link rel="canonical" href={`${DOMAIN}/blogs/${query.slug}`} />
+            <meta property="og:title" content={`${blog.title}| ${APP_NAME}`} />
+            <meta property="og:description" content={blog.mdesc} />
+            <meta property="og:type" content="webiste" />
+            <meta property="og:url" content={`${DOMAIN}/blogs/${query.slug}`} />
+            <meta property="og:site_name" content={`${APP_NAME}`} />
+
+            <meta property="og:image" content={`${API}/blog/photo/${blog.slug}`} />
+            <meta property="og:image:secure_url" ccontent={`${API}/blog/photo/${blog.slug}`} />
+            <meta property="og:image:type" content="image/jpg" />
+
+        </Head>
+    );
 
     const showBlogCategories = blog =>
         blog.categories.map((c, i) => (
@@ -23,63 +59,64 @@ const SingleBlog = ({ blog, router }) => {
             </Link>
         ));
 
+    const showRelatedBlog = () => {
+        return related.map((blog, i) => (
+            <div className="col-md-4" key={i}>
+                <article>
+                    <SmallCard blog={blog} />
+                </article>
+            </div>
+        ));
+    };
 
     return (
         <React.Fragment>
+            {head()}
             <Layout>
                 <main>
                     <article>
                         <div className="container-fluid">
-
                             <section>
-
-
-
-                                <div className="row" style={{ marginTop: '-20px' }}>
-
-                                    <img src={`${API}/blog/photo/${blog.slug}`} alt={blog.title} className="img img-fluid featured-image" />
-
+                                <div className="row" style={{ marginTop: '-30px' }}>
+                                    <img
+                                        src={`${API}/blog/photo/${blog.slug}`}
+                                        alt={blog.title}
+                                        className="img img-fluid featured-image"
+                                    />
                                 </div>
-
                             </section>
 
                             <section>
+                                <div className="container">
+                                    <h1 className="display-2 pb-3 pt-3 text-center font-weight-bold">{blog.title}</h1>
+                                    <p className="lead mt-3 mark">
+                                        Written by {blog.postedBy.name} | Published {moment(blog.updatedAt).fromNow()}
+                                    </p>
 
-                                <p className="lead mt-3 mark">
-
-                                    Written by {blog.postedBy.name} | Published {moment(blog.updatedAt).fromNow()}
-                                </p>
-
-                                <div className="pb-3">
-
-                                    {showBlogCategories(blog)}
-                                    {showBlogTags(blog)}
-                                    <br />
-                                    <br />
-
+                                    <div className="pb-3">
+                                        {showBlogCategories(blog)}
+                                        {showBlogTags(blog)}
+                                        <br />
+                                        <br />
+                                    </div>
                                 </div>
-
                             </section>
-
                         </div>
 
                         <div className="container">
                             <section>
-                                <div className="col-md-12 lead">
-                                    {renderHTML(blog.body)}
-                                </div>
+                                <div className="col-md-12 lead">{renderHTML(blog.body)}</div>
                             </section>
+                        </div>
 
+                        <div className="container">
+                            <h4 className="text-center pt-5 pb-5 h2">Related blogs</h4>
+                            <div className="row">{showRelatedBlog()}</div>
                         </div>
 
                         <div className="container pb-5">
-                            <h4 className="text-center pb-5 pt-5 h2">Related Blogs</h4>
                             <p>show comments</p>
-
                         </div>
-
-
-
                     </article>
                 </main>
             </Layout>
@@ -93,9 +130,9 @@ SingleBlog.getInitialProps = ({ query }) => {
             console.log(data.error);
         } else {
             // console.log('GET INITIAL PROPS IN SINGLE BLOG', data);
-            return { blog: data };
+            return { blog: data, query };
         }
     });
 };
 
-export default (SingleBlog);
+export default SingleBlog;
