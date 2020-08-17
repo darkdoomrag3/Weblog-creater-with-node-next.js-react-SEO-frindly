a65bdc0389bb2665643ddcd33dccc0e47ff198f3
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-08-17 16:20:46 +0430

    List all created blog by user

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -108,7 +108,7 @@ exports.adminMiddleware = (req, res, next) => {
 
 exports.canUpdateDeleteBlog = (req, res, next) => {
     const slug = req.params.slug.toLowerCase();
-    Blog.finOne({ slug }).exec((err, data) => {
+    Blog.findOne({ slug }).exec((err, data) => {
         if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
